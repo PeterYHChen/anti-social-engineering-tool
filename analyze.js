359a82d735af1bdcdadda51166bffba7d0bc475a
yonghong <yonghong.chenn@gmail.com>
2017-11-29 00:40:51 -0500

    Put image graph and severity bar to popup

--- analyze.js
+++ analyze.js
@@ -1,10 +1,6 @@
-// console.log('hello');
-var links = document.getElementsByTagName("a");
-console.log(links);
 
-var domainMap = {};
-var validLinkCount = 0;
-var popupLinkPageHTML = chrome.extension.getURL("popup-link.html");
+var domainMap = getDomainMapFromDoc(document);
+var links = document.getElementsByTagName("a");
 for (let i = 0; i < links.length; i++) {
     links[i].addEventListener("mouseenter", () => {
         showPopup(i, links[i].href);
@@ -12,27 +8,15 @@ for (let i = 0; i < links.length; i++) {
     links[i].addEventListener("mouseleave", () => {
         hidePopup(i);
     });
-    links[i].innerHTML += "<div class=\"popup-box\" id=\"popup-link-" + i + "\">A Simple Popup!</div>";
-    let rootDomain = extractRootDomain(links[i].href);
-
-    // Skip empty string
-    if (rootDomain == null || rootDomain == '') {
-        continue;
-    }
-
-    // Add to the map for counting
-    if (domainMap[rootDomain] == null) {
-        domainMap[rootDomain] = 0;
-    }
-    domainMap[rootDomain] = domainMap[rootDomain] + 1;
-    validLinkCount++;
-
+    links[i].innerHTML += "<div class=\"popup-box\" id=\"popup-link-" + i + "\">" +
+        "<meter id=\"severity-meter-" + i + "\" class=\"severity-bar\" min=\"0\" low=\"40\" high=\"75\" max=\"100\" value=\"0\" optimum=\"90\"></meter>" +
+        "<div id=\"severity-" + i + "\"></div>" +
+        "<canvas id=\"pieChart-" + i + "\" width=\"500\" height=\"500\"></canvas>" +
+        "</div>";
     links[i].title = links[i].href;
+
     let wordsInLink = links[i].href.split(/[^A-Za-z]/);
     let wordsInLinkSet = new Set(wordsInLink);
-    links[i].onclick = function () {
-        console.log(wordsInLinkSet);
-    };
 
     if (links[i].href != links[i].innerText) {
         // wordsInLink = links[i].href.split(/[^A-Za-z]/);
@@ -49,6 +33,27 @@ chrome.runtime.sendMessage({
     source: domainMap
 });
 
+function getDomainMapFromDoc(htmlDoc) {
+    var links = htmlDoc.getElementsByTagName("a");
+    console.log(links);
+
+    var domainMap = {};
+    for (let i = 0; i < links.length; i++) {
+        let rootDomain = extractRootDomain(links[i].href);
+
+        // Skip empty string
+        if (rootDomain == null || rootDomain == '') {
+            continue;
+        }
+
+        // Add to the map for counting
+        if (domainMap[rootDomain] == null) {
+            domainMap[rootDomain] = 0;
+        }
+        domainMap[rootDomain] = domainMap[rootDomain] + 1;
+    }
+    return domainMap;
+}
 
 function extractHostname(url) {
     var hostname;
@@ -95,18 +100,21 @@ function showPopup(index, url) {
     popup.style.display = "block";
 
     var request = makeHttpObject();
-    request.open("GET", url, true);
+    request.open("GET", "https://anti-social-engineering-tool.mybluemix.net/page?url=" + url, true);
     request.send(null);
     request.onreadystatechange = function () {
-        if (request.readyState == 4)
-            alert(request.responseText);
-        // popup.innerHTML = request.responseText;
+        if (request.readyState == 4) {
+            // console.log(request);
+            var dummyDoc = document.createElement('dummyDoc');
+            dummyDoc.innerHTML = request.responseText;
+            var domainMap = getDomainMapFromDoc(dummyDoc);
+            drawChart(domainMap, index, url);
+        }
     };
 }
 
 function hidePopup(index) {
     console.log(index);
-    // this.style.color = "black";
 
     var popup = document.getElementById("popup-link-" + index);
     popup.style.display = "none";
@@ -121,4 +129,69 @@ function makeHttpObject() {
     catch (error) { }
 
     throw new Error("Could not create HTTP request object.");
+}
+
+
+function drawChart(domainMap, index, url) {
+    // Calculate the sum of total links
+    let validLinkCount = 0;
+    for (let domain in domainMap) {
+        validLinkCount += domainMap[domain];
+    }
+
+    // Transform domain map to percentage
+    let domainPercentMap = {};
+    for (let domain in domainMap) {
+        domainPercentMap[domain] = (domainMap[domain] * 100 / validLinkCount).toFixed(1);
+    }
+
+    var domainData = [];
+    var domains = [];
+    var counts = [];
+    for (let domain in domainPercentMap) {
+        domains.push(domain + "(" + domainPercentMap[domain] + "%)");
+        counts.push(domainMap[domain]);
+    }
+
+    var backgroundColor = [];
+    var hoverBackgroundColor = [];
+
+    for (let i = 0; i < domains.length; i++) {
+        r = Math.floor(Math.random() * 200);
+        g = Math.floor(Math.random() * 200);
+        b = Math.floor(Math.random() * 200);
+        v = Math.floor(Math.random() * 500);
+        c = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+        h = 'rgb(' + (r + 20) + ', ' + (g + 20) + ', ' + (b + 20) + ')';
+        backgroundColor.push(c);
+        hoverBackgroundColor.push(h);
+    }
+
+    var pieChartData = {};
+    pieChartData.labels = domains;
+    pieChartData.datasets = [];
+    pieChartData.datasets.push({
+        data: counts,
+        backgroundColor: backgroundColor,
+        hoverBackgroundColor: hoverBackgroundColor
+    });
+
+    console.log(pieChartData);
+    var canvas = document.getElementById("pieChart-" + index);
+    var ctx = canvas.getContext("2d");
+
+    var myChart = new Chart(ctx, {
+        type: 'pie',
+        data: pieChartData,
+    });
+
+    // Set severity by comparing the current url with the one in the chart
+    let currDomain = extractRootDomain(url);
+    let currDomainPercent = domainPercentMap[currDomain];
+    if (currDomainPercent == null) {
+        currDomainPercent = 0;
+    }
+
+    document.getElementById("severity-" + index).innerHTML = currDomainPercent + "% of the links in this websites are safe.";
+    document.getElementById("severity-meter-" + index).value = currDomainPercent;
 }
