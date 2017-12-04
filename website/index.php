4604521749d1a058c515cfe1297934dc6724dcc2
Ashkara <luck.michael@nyu.edu>
2017-12-04 18:02:09 -0500

    Update website.

---
+++ website/index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<head>
+  <title>ASET</title>
+  <link rel="stylesheet" type="text/css" href="book.css">
+  <link rel="stylesheet" type="text/css" href="shelf.css">
+  <link rel="stylesheet" type="text/css" href="book_on_shelf.css">
+  <link rel="stylesheet" type="text/css" href="master.css"
+  </head>
+  <body>
+    <div id="tridiv">
+      <div class="scene">
+        <div class="shape cuboid shelf">
+          <div class="face ft">
+            <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.203922);"></div>
+          </div>
+          <div class="face bk">
+            <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.592157);"></div>
+          </div>
+          <div class="face rt">
+            <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.2);"></div>
+          </div>
+          <div class="face lt">
+            <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.203922);"></div>
+          </div>
+          <div class="face bm">
+            <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.690196);"></div>
+          </div>d
+          <div class="face tp">
+            <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.109804);"></div>
+          </div>
+        </div>
+        <figure class='book' id="CIMS">
+          <!-- Front -->
+          <ul class='hardcover_front'>
+            <li>
+              <a class="btn" href="book_A.html"><img src="https://www.math.nyu.edu/~crorres/awc/graphics_conf/wwhall_photo.jpg" alt="" width="100%" height="100%"></a>
+              <span class="ribbon bestseller">Nº1</span>
+            </li>
+            <li>adsf</li>
+          </ul>
+          <!-- Pages -->
+          <ul class='page'>
+            <li></li>
+            <li>
+              <a class="btn" href="book_A.html">CIMS<br>The Courant
+                <br>Institute of
+                <br>Mathematical
+              <br>Sciences</a>
+            </li>
+            <li></li>
+            <li></li><li></li>
+          </ul>
+          <!-- Back -->
+          <ul class='hardcover_back'>
+            <li></li>
+            <li></li>
+          </ul>
+          <ul class='book_spine'>
+            <li></li>
+            <li></li>
+          </ul>
+        </figure>
+        
+        <figure class='book' id="AUTHORS">
+          <!-- Front -->
+          <ul class='hardcover_front'>
+            <li>
+              <a class="btn" href="book_A.html"><img src="https://www.math.nyu.edu/~crorres/awc/graphics_conf/wwhall_photo.jpg" alt="" width="100%" height="100%"></a></li>
+              <li></li>
+            </ul>
+            <!-- Pages -->
+            <ul class='page'>
+              <li></li>
+              <li>
+                <a class="btn" href="#"> <br>MICHAEL LUKIMAN
+                  <br>
+                <br></a>
+              </li>
+              <li></li>
+              <li></li><li></li>
+            </ul>
+            <!-- Back -->
+            <ul class='hardcover_back'>
+              <li></li>
+              <li></li>
+            </ul>
+            <ul class='book_spine'>
+              <li></li>
+              <li></li>
+            </ul>
+          </figure>
+          
+          <div class="shape cuboid shelf">
+            <div class="face ft">
+              <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.203922);"></div>
+            </div>
+            <div class="face bk">
+              <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.592157);"></div>
+            </div>
+            <div class="face rt">
+              <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.2);"></div>
+            </div>
+            <div class="face lt">
+              <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.203922);"></div>
+            </div>
+            <div class="face bm">
+              <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.690196);"></div>
+            </div>
+            <div class="face tp">
+              <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.109804);"></div>
+            </div>
+          </div>
+          
+          <div class="shape cuboid shelf">
+            <div class="face ft">
+              <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.203922);"></div>
+            </div>
+            <div class="face bk">
+              <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.592157);"></div>
+            </div>
+            <div class="face rt">
+              <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.2);"></div>
+            </div>
+            <div class="face lt">
+              <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.203922);"></div>
+            </div>
+            <div class="face bm">
+              <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.690196);"></div>
+            </div>
+            <div class="face tp">
+              <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.109804);"></div>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </body>
---
+++ website/shelf_book_B.php
@@ -0,0 +1,28 @@
+        <figure class='book' id="AUTHORS">
+          <!-- Front -->
+          <ul class='hardcover_front'>
+            <li>
+              <a class="btn" href="book_A.html"><img src="https://www.math.nyu.edu/~crorres/awc/graphics_conf/wwhall_photo.jpg" alt="" width="100%" height="100%"></a></li>
+              <li></li>
+            </ul>
+            <!-- Pages -->
+            <ul class='page'>
+              <li></li>
+              <li>
+                <a class="btn" href="#"> <br>MICHAEL LUKIMAN
+                  <br>
+                <br></a>
+              </li>
+              <li></li>
+              <li></li><li></li>
+            </ul>
+            <!-- Back -->
+            <ul class='hardcover_back'>
+              <li></li>
+              <li></li>
+            </ul>
+            <ul class='book_spine'>
+              <li></li>
+              <li></li>
+            </ul>
+          </figure>
